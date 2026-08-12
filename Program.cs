2f201ecbc1a65b2419562ff1c0ec063a764a6d81
Tuan <www.2000tongvantuan@gmail.com>
2026-08-12 15:08:28 +0200

    Temp: Enable detailed errors for production debugging

--- Program.cs
+++ Program.cs
@@ -37,9 +37,9 @@ builder.Services.AddScoped<ChangelogService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseDeveloperExceptionPage();
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
